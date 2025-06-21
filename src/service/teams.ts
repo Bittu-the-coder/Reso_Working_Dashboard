f734037feee2153f76190a3634c8432207a98bc8
Bittu-the-coder <Bittuprajapati2271@gmail.com>
2025-06-21 22:09:09 +0530

    change

--- src/service/teams.ts
+++ src/service/teams.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API_BASE_URL = "http://localhost:3030/api";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:3030/api";
 
 // Create axios instance
 const apiClient = axios.create({
