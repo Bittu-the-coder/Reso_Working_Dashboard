d230f0f15c75c4cad96cce3668ee4d416a011036
Bittu-the-coder <Bittuprajapati2271@gmail.com>
2025-07-02 19:35:21 +0530

    change

--- backend/src/app.js
+++ backend/src/app.js
@@ -64,6 +64,15 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/api', (req, res, next) => {
+  res.set({
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Origin': 'https://reso-working-dashboard.vercel.app',
+    'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Origin, Authorization'
+  });
+  next();
+});
 
 
 // Health check endpoint with more debug info
